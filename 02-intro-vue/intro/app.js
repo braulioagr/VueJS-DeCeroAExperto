63e56870ea6d065f1c74805a11f3a88e586939ac
braulioagr <braulio.code@gmail.com>
2024-07-08 23:08:30 -0600

    feat: finished section 1 & 2

---
+++ 02-intro-vue/intro/app.js
@@ -0,0 +1,29 @@
+
+const { createApp, ref } = Vue
+const app = createApp({
+  // template: `
+  //   <h1>{{message}}</h1>
+  //   <p> - {{author}}</p>
+  // `,
+  setup(){
+    const message = ref("I'm batman");
+    const author = ref("Bruce Wayne");
+    // setTimeout(() => {
+    //   message.value = "Hola, soy Goku";
+    //   author.value = "Goku";
+    // },1500)
+
+    const changeQuote = () => {
+      message.value = "Hola, soy Goku";
+      author.value = "Goku";
+    }
+
+    return {
+      message,
+      author,
+      changeQuote
+    };
+  }
+});
+
+app.mount('#myApp');
